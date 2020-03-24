f65f67b96b1d888e8f852d4eba34fa9fb7b0600c
JorgeLNJunior <jorgelnjunir@gmail.com>
2020-03-24 16:23:29 -0300

    Implementar o registro de usuários

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const User = use('App/Models/User')
+const { validate } = use('Validator')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/auth/src/Schemes/Session')} AuthSession */
+
+/**
+ * Controller to register, authenticate and activate user accounts
+ */
+
+class AuthController {
+
+  /**
+   * Register a user
+   * POST /register
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async register({request, response}) {
+
+    const { username, email, password } = request.body
+
+    const rules = {
+      username: 'required|unique:users|min:3|max:30',
+      email: 'required|email|unique:users',
+      password: 'required|min:6|max:20'
+    }
+
+    const validation = await validate(request.body, rules)
+
+    if(validation.fails()) {
+      return response.status(422).json({ errors: validation.messages() })
+    }
+
+    const user = await User.create({
+      username: username,
+      password: password,
+      email: email
+    })
+
+    return response.status(201).json({ message: 'user registred', user: user })
+
+  }
+}
+
+module.exports = AuthController
